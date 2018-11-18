06ed5bce4048dd32bb38641f68e659d1061d9c74
Hariharan Subramanian <hariharan.subramanian@ivanti.com>
2018-11-18 21:32:54 +0530

    Handling selection and preview of image from PC

--- imagesnipper/src/App.js
+++ imagesnipper/src/App.js
@@ -3,26 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: '',
+      imageSelected: ''
+    };
+    this._handleImageChange = this._handleImageChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imageSelected: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
   render() {
+    let { imageSelected } = this.state;
+    let $imagePreviewDiv = null;
+
+    if (imageSelected) {
+      $imagePreviewDiv = (<img src={imageSelected} height="70%" width="70%" />);
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <form onSubmit={this._handleSubmit}>
+              <input type="file" onChange={this._handleImageChange} />
+              <button type="submit" disabled={!$imagePreviewDiv} onClick={this._handleSubmit}>Crop Border</button>
+            </form>
+            <div >
+              {$imagePreviewDiv}
+            </div>
+          </div>
         </header>
       </div>
-    );
-  }
-}
 
+    )
+  }
+
+}
 export default App;
